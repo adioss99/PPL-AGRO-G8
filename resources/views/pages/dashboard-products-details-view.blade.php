ac36bd5658facebee85c10b5d104b4067305bb7a
Your Name <adisasmito1104@gmail.com>
2022-05-20 19:18:08 +0700

    request product

--- resources/views/pages/dashboard-products-details-view.blade.php
+++ resources/views/pages/dashboard-products-details-view.blade.php
@@ -4,71 +4,101 @@
     Detail Produk
 @endsection
 
+@push('modal')
+{{-- Chat WA --}}
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Custom Packing</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>Anda harus menghubungi admin untuk mengedit data produk</p>
+      </div>
+      <div class="modal-footer">
+        <a type="button" class="btn btn-success"href="https://wa.me/+628113780510?text=*Edit%20Produk*%0Aid%20produk%3A%20{{$product->id}}%20" target="_blank">Hubungi Admin <i class="fa-brands fa-whatsapp"></i></a>
+      </div>
+    </div>
+  </div>
+</div>
+@endpush
+
 @section('content')
 <div class="section-content section-dashboard-home" data-aos="fade-up">
-    <div class="container-fluid">
-        <div class="dashboard-heading">
-            <h2 class="dashboard-title">Detail Produk</h2>
-        </div>
-        <div class="page-content page-details">
-          <section class="store-gallery mb-3" id="gallery">
-            <div class="container">
-              <div class="row mt-4">
-                <div class="col-lg-8" data-aos="zoom-in">
-                  <transition name="slide-fade" mode="out-in">
-                    <img 
-                    :key="photos[activePhoto].id" 
-                    :src="photos[activePhoto].url" 
-                    class="w-100 main-image" alt="" />
-                  </transition>
-                </div>
-                <div class="col-lg-2">
-                  <div class="row">
-                    <div class="col-3 col-lg-12 mt-2 mt-lg-0" v-for="(photo, index) in photos" :key="photo.id" data-aos="zoom-in" data-aos-delay="100">
-                      <a href="#" @click="changeActive(index)">
-                        <img :src="photo.url" class="w-100 thumbnail-image" :class="{ active: index == activePhoto }" alt="" />
-                      </a>
-                    </div>
+  <div class="container-fluid">
+    <div class="dashboard-heading">
+        <h2 class="dashboard-title">Detail Produk</h2>
+    </div>   
+    <div class="row mt-4">
+      <div class="page-details">
+        <section class="store-gallery mb-3" id="gallery">
+          <div class="container">
+            <button
+                data-toggle="modal" data-target="#exampleModal"
+                class="btn btn-success mt-4"
+                ><i class="bi bi-pencil-square"></i> Edit Produk
+            </button>
+            <div class="row mt-4">
+              <div class="col-lg-8" data-aos="zoom-in">
+                <transition name="slide-fade" mode="out-in">
+                  <img 
+                  :key="photos[activePhoto].id" 
+                  :src="photos[activePhoto].url" 
+                  class="w-100 main-image" alt="" />
+                </transition>
+              </div>
+              <div class="col-lg-2">
+                <div class="row">
+                  <div class="col-3 col-lg-12 mt-2 mt-lg-0" v-for="(photo, index) in photos" :key="photo.id" data-aos="zoom-in" data-aos-delay="100">
+                    <a href="#" @click="changeActive(index)">
+                      <img :src="photo.url" class="w-100 thumbnail-image" :class="{ active: index == activePhoto }" alt="" />
+                    </a>
                   </div>
                 </div>
               </div>
             </div>
-          </section>
-          <div class="store-details-container" data-aos="fade-up">
-            <section class="store-heading">
-              <div class="container">
-                <div class="row">
-                  <div class="col-lg-8">
-                    <h1>{{$product->name}}</h1>
-                    <div class="price">{{Str::rupiah($product->price)}}</div>
-                  </div>
-                  <div class="col-lg-2" data-aos="zoom-in">
-                    <form action="{{route('cart-add',$product->id)}}" method="POST" enctype="multipart/form-data">
-                      @csrf
-                      <button
-                        type="submit"
-                        class="btn btn-success px-4 text-white btn-block mb-3"
-                      >
-                        Add to Cart
-                      </button>
-                    </form>
-                  </div>
-                </div>
-              </div>
-            </section>
-            <section class="store-description">
-              <div class="container mb-5">
-                <div class="row">
-                  <div class="col-12 col-lg-8">
-                    {!! $product->description !!}
-                  </div>
-                </div>
-              </div>
-            </section>
           </div>
+        </section>
+        <div class="store-details-container" data-aos="fade-up">
+          <section class="store-heading">
+            <div class="container">
+              <div class="row">
+                <div class="col-lg-8">
+                  <h1>{{$product->name}}</h1>
+                  <div class="price">{{Str::rupiah($product->price)}}</div>
+                </div>
+                <div class="col-lg-2" data-aos="zoom-in">
+                  <form action="{{route('cart-add',$product->id)}}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <button
+                      type="submit"
+                      class="btn btn-success px-4 text-white btn-block mb-3"
+                    >
+                      Add to Cart
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </section>
+          <section class="store-description">
+            <div class="container mb-3">
+              <div class="row">
+                <div class="col-12 col-lg-8">
+                  Berat Produk: {{$product->weight}} Kg <br>
+                  {!! $product->description !!}
+                </div>
+              </div>
+            </div>
+          </section>
         </div>
-      </div>
+      </div>           
     </div>
+  </div>
+</div>
 @endsection
 
 @push('addon-script')
--- resources/views/pages/dashboard-products.blade.php
+++ resources/views/pages/dashboard-products.blade.php
@@ -6,16 +6,47 @@
     Produk
 @endsection
 
+@push('modal')
+{{-- Chat WA --}}
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Custom Packing</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>Anda harus menghubungi admin untuk menambahkan produk baru</p>
+      </div>
+      <div class="modal-footer">
+        <a type="button" class="btn btn-success"href="https://wa.me/+628113780510?text=*Request%20Produk*%0Aid%20pelanggan%3A%20{{auth()->user()->id}}%20" target="_blank">Hubungi Admin <i class="fa-brands fa-whatsapp"></i></a>
+      </div>
+    </div>
+  </div>
+</div>
+@endpush
+
 @section('content')
 <div class="section-content section-dashboard-home" data-aos="fade-up">
     <div class="container-fluid">
         <div class="dashboard-heading">
             <h2 class="dashboard-title">Produk</h2>
             <p class="dashboard-subtitle">
-                <br />
+                <br>
             </p>
         </div>
         <div class="dashboard-content">
+            <div class="row">
+                <div class="col-12">
+                <button
+                    data-toggle="modal" data-target="#exampleModal"
+                    class="btn btn-success"
+                    ><i class="fa-solid fa-plus"></i> Request Produk</button
+                >
+                </div>
+            </div>
             <div class="row mt-4">
                 @foreach ($products  as $product)
                 <div class="col-12 col-sm-6 col-md-4 col-lg-3">
